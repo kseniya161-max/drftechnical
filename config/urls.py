c5b161861b680d1baaea7d57566a427e87b61787
Kseniya <baharevaxen@yandex.ru>
2025-12-20 22:02:00 +0300

    create model User

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls')),
+    path('api/tasks/', include('tasks.urls')),
+    path('api/users/', include('tasks.urls')),
 ]
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель Пользователя"""
+    email = models.EmailField(unique=True, verbose_name ='Ваш Email')
+    name = models.CharField(max_length=100, verbose_name ='Имя Пользователя', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
