c19a84dc422e3e2553010a3246ac58da3ac360f5
Kseniya <baharevaxen@yandex.ru>
2025-12-21 17:31:18 +0300

    correct login and logout for author/ user

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from users.views import UserViewSet, UserCreateAPIView, registration_view, login_view
+from users.views import UserViewSet, UserCreateAPIView, registration_view, login_view, logout_view
 
 app_name = "users"
 router = SimpleRouter()
@@ -12,5 +12,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register/', registration_view, name='register'),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
