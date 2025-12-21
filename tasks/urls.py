6c29a83561bbd1addaaebc6f127e29f73a7d4ac0
Kseniya <baharevaxen@yandex.ru>
2025-12-21 13:59:22 +0300

    creating detail

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,7 @@
 from os import path
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from tasks.views import TaskViewSet, task_list
+from tasks.views import TaskViewSet, task_list, task_detail
 
 app_name = "tasks"
 
@@ -11,4 +11,6 @@ router.register(r"tasks", TaskViewSet)
 urlpatterns = [
     path('', task_list, name='task_list'),
     path('api/', include(router.urls)),
+    path('task/<int:task_id>/', task_detail, name='task_detail'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -19,8 +20,17 @@ class TaskViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         serializer.save()
 
+    def perform_delete(self, serializer):
+        serializer.save()
+
+    def retrieve(self, request, *args, **kwargs):
+        """Получение задачи по ID"""
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
 def task_list(request):
-    """Получаем все задачи и передаем в шаблон"""
+    """Получаем, удаляем, создаем все задачи и передаем в шаблон"""
     tasks = Task.objects.all()
     if request.method == 'POST':
         if 'header' in request.POST:
@@ -31,8 +41,17 @@ def task_list(request):
         elif 'task_id' in request.POST:
             task_id = request.POST.get('task_id')
             task = Task.objects.get(id=task_id)
-            task.completed = not task.completed
-            task.save()
+            if 'delete_task' in request.POST:
+                task.delete()
+            else:
+                task.completed = not task.completed
+                task.save()
             return redirect('tasks:task_list')
 
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+def task_detail(request, task_id):
+    """Получаем задачу по ID и передаем в шаблон"""
+    task = get_object_or_404(Task, id=task_id)
+    return render(request, 'tasks/task_detail.html', {'task': task})
