8e0d201e0e705b0c15f209f977220d90cc79eda9
Kseniya <baharevaxen@yandex.ru>
2025-12-21 11:45:41 +0300

    create view and template for creating task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,7 @@
 from os import path
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from tasks.views import TaskViewSet, task_list
+from tasks.views import TaskViewSet, task_list, create_task
 
 app_name = "tasks"
 
@@ -10,6 +10,6 @@ router.register(r"tasks", TaskViewSet)
 
 urlpatterns = [
     path('', task_list, name='task_list'),
+    path('create/', create_task, name='task_create'),
     path('api/', include(router.urls)),
 ]
-
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
@@ -24,3 +26,11 @@ def task_list(request):
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
 
+@api_view(['POST'])
+def create_task(request):
+    """Создание Задачи"""
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
