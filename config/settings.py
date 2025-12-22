3374a7f3831be5af14b7a7f1a17e9a11b8203999
Kseniya <baharevaxen@yandex.ru>
2025-12-23 00:42:01 +0300

    url

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 import os
 load_dotenv()
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.getenv("DATABASE_NAME"),
+#         "USER": os.getenv("DATABASE_USER"),
+#         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+#         "HOST": os.getenv("DATABASE_HOST"),
+#         "PORT": os.getenv("DATABASE_PORT", default="5432"),
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
